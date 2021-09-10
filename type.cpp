6e8354fc3cf104b144f94cf22071d8f8987607bc
shashank-16 <shashank.chaurasia1606@gmail.com>
2021-09-11 03:49:19 +0530

    finally quiting the program is working

--- type.cpp
+++ type.cpp
@@ -1,20 +1,35 @@
 //include section
 
 #include<ctype.h>
+#include<cstdio>
 #include<errno.h>
 #include<iostream>
-#include<cstdio>
-#include<unistd.h>
 #include<termios.h>
 #include<stdlib.h>
+#include<sys/ioctl.h>
+#include<unistd.h>
+
+
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 //data section 
+struct editorConfig{
+
+    int screenrows;
+    int screencols;
+
 struct termios orig_termios;
+
+};
+
+struct editorConfig E;
 
 //terminal section 
 
 void die(const char* s)
 {
+    write(STDOUT_FILENO,"\x1b[2J",4);
+    write(STDOUT_FILENO,"\x1b[H",3);
     perror(s);
     exit(1);
 
@@ -22,7 +37,7 @@ void die(const char* s)
 
 void disableRawMode()
 {
-    if( tcsetattr(STDIN_FILENO,TCSAFLUSH, &orig_termios) ==-1)
+    if( tcsetattr(STDIN_FILENO,TCSAFLUSH, &E.orig_termios) ==-1)
     {
         die("tcsetattr");
 
@@ -34,7 +49,7 @@ void disableRawMode()
 
 void enableRawMode(){
 
-    if (tcgetattr(STDIN_FILENO,&orig_termios)== -1)
+    if (tcgetattr(STDIN_FILENO,&E.orig_termios)== -1)
     {
         die("tcgetattr");
 
@@ -42,7 +57,7 @@ void enableRawMode(){
     atexit(disableRawMode);
 
 
-    struct termios raw= orig_termios;
+    struct termios raw= E.orig_termios;
     raw.c_iflag &=~(IXON | ICRNL|BRKINT|INPCK);
     raw.c_oflag &=~(OPOST);
     raw.c_cflag |= (CS8);
@@ -57,6 +72,112 @@ void enableRawMode(){
 
 }
 
+char editorReadkey()
+{
+    int nread;
+    char a;
+    while((nread = read(STDIN_FILENO,&a,1 ))!=1)
+    {
+        if(nread == -1 && errno !=EAGAIN ) die ("read");
+
+    }
+    return a;
+
+}
+
+void editorProcessKeypress()
+{
+    char c= editorReadkey();
+
+    switch(c)
+    {
+        case CTRL_KEY('q'):
+        write(STDOUT_FILENO,"\x1b[2J",4);
+        write(STDOUT_FILENO,"\x1b[H",3);
+        exit(0);
+        break;
+
+    }
+}
+
+void editorDrawRows()
+{
+    int y;
+    for(y=0;y<E.screenrows;y++)
+    {
+        write(STDOUT_FILENO,"~~",2); // increase these to add symbol at first line of rows 
+
+        if(y<E.screenrows -1 )
+        {
+            write(STDOUT_FILENO,"\r\n",2);
+
+        }
+
+    }
+}
+
+void editorrefreshScreen()
+{
+    write(STDOUT_FILENO,"\x1b[2J",4);
+    write(STDOUT_FILENO,"\x1b[H",3);
+    editorDrawRows();
+    write(STDOUT_FILENO,"\x1b[H",3);
+
+
+
+
+}
+
+int getCursorPosition(int* rows ,int* cols)
+{
+    char buf[32];
+    unsigned int i=0;
+
+    if(write(STDOUT_FILENO,"\x1b[6n",4)!=4) return -1;
+
+  while(i<sizeof(buf)-1)
+  {
+      if(read(STDIN_FILENO,&buf[i],1)!=1)break;
+      if(buf[i]=='R') break;
+      i++;
+
+  }
+buf[i]='\0';
+
+if(buf[0] != '\x1b' || buf[1]!= '[') return -1;
+if(sscanf(&buf[2], "%d;%d",rows,cols) !=2 )return -1;
+
+
+    //   printf("\r\n&buf[1]: '%s'\r\n",&buf[1]);
+
+    // editorReadkey();
+    return 0;
+
+}
+
+int getWindowSize(int* rows,int* cols )
+{
+    struct winsize ws;
+    if(ioctl(STDOUT_FILENO,TIOCGWINSZ,&ws)== -1 || ws.ws_col==0)
+    {
+        if(write(STDOUT_FILENO,"\x1b[999C\x1b[999B",12) != 12 ) return -1;
+        // editorReadkey(); //  i think something will happen here <---------------------------------------------------------------
+        getCursorPosition(rows,cols);
+        return -1;
+
+    }else{
+        *cols=ws.ws_col;
+        *rows=ws.ws_row;
+        return 0;
+
+    }
+}
+
+
+void initEditor()
+{
+    if(getWindowSize(&E.screenrows,&E.screencols)== -1 ) die("getWindowSize");
+}
 //init 
 
 int main()
@@ -64,28 +185,15 @@ int main()
    
     // std::cout<<"        ......././././.. Thanks for Downlaoding type Text editor ...././././././";
 
-     enableRawMode();
+    enableRawMode();
+    initEditor();
 
 
     
     while(1){
-        char c ='\0';
+        editorrefreshScreen();
 
-       if (read(STDIN_FILENO,&c,1)==-1 && errno != EAGAIN ) die ("read");
-
-        if(iscntrl(c))
-        {
-            
-            printf("%d\r\n",c);
-
-        }
-        else{
-           printf("%d('%c')\r\n",c,c);
-
-
-        }
-     if(c=='q') break;
-
+      editorProcessKeypress();
 
     }
 
